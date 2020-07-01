3d1c1e44dfbb7fc3855f3d3f9edda1ccb0de187c
tjamhar <t-jamhar@microsoft.com>
2020-07-01 14:46:37 -0700

    added display filter

--- epan/dissectors/packet-ippusb.c
+++ epan/dissectors/packet-ippusb.c
@@ -25,10 +25,18 @@
 #include "packet-usb.h"
 #include <stdio.h>
 
+#define HTTP 0
+#define IPP 1
+
 void proto_register_ippusb(void);
 void proto_reg_handoff_ippusb(void);
 
 static int proto_ippusb = -1;
+static gint ett_ippusb = -1;
+static gint ett_ippusb_as = -1;
+static gint ett_ippusb_attr = -1;
+static gint ett_ippusb_member = -1;
+
 static dissector_table_t ippusb_dissector_table;
 
 static int
@@ -36,55 +44,83 @@ dissect_ippusb(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
 {
     //skip to http header info
     //put dummy info in for http dissector
+    proto_tree  *ippusb_tree;
+    proto_item  *ti;
+    int offset = 0;
+    int ret;
+    int first_linelen;
+    const guchar *first_line;
+    gint next_offset;
 
-    proto_tree  *ippusb_tree = tree;
-    //proto_item  *ti;
-    int         offset     = 0;
-    //http_message_info_t *message_info = (http_message_info_t *)data;
-    //gboolean    is_request;
-    //guint16     operation_status;
-   // const gchar *status_type;
-    //guint32	request_id;
-    //conversation_t *conversation;
+    ti = proto_tree_add_item(tree, proto_ippusb, tvb, offset, -1, 0);
+    ippusb_tree = proto_item_add_subtree(ti, ett_ippusb);
 
-    //offset +=
+    first_linelen = tvb_find_line_end(tvb, offset,  tvb_ensure_captured_length_remaining(tvb, offset), &next_offset, TRUE);
+    first_line = tvb_get_ptr(tvb, offset, first_linelen);
 
-    if (tvb_captured_length_remaining(tvb, offset) > 0) {
-        next_tvb = tvb_new_subset_remaining(tvb, offset);
-        offset += try_dissect_next_protocol(parent, next_tvb, pinfo, usb_conv_info, urb_type, tree, NULL);
+    //check if there is a http header to parse before ipp
+    if (is_http_header(first_linelen, first_line)) {        
+        ret = dissector_try_uint_new(ippusb_dissector_table, HTTP, tvb, pinfo, tree, TRUE, data);
+    }
+    else {
+        ret = dissector_try_uint_new(ippusb_dissector_table, IPP, tvb, pinfo, tree, TRUE, data);
     }
 
-    if (tvb_captured_length_remaining(tvb, offset) > 0) {
-        /* There is still leftover capture data to add (padding?) */
-        proto_tree_add_item(parent, hf_usb_capdata, tvb, offset, -1, ENC_NA);
+    if (ret) {
+            return tvb_captured_length(tvb);
     }
+    else {
+        return 0;
+    }
+}
 
-    if (tvb_offset_exists(tvb, offset) && data) {
-        call_data_dissector(tvb_new_subset_remaining(tvb, offset), pinfo, ippusb_tree);
+bool is_http_header(int first_linelen, const guchar *first_line){
+    if ((first_linelen >= 5 && strncmp(firstline, "HTTP/", 5) == 0) ||
+		(first_linelen >= 3 && strncmp(firstline, "ICY", 3) == 0) ||
+        (first_linelen >= 3 && strncmp(firstline, "GET", 3) == 0) ||
+		(first_linelen >= 3 && strncmp(firstline, "PUT", 3) == 0) ||
+        (first_linelen >= 4 && strncmp(firstline, "COPY", 4) == 0) ||
+		(first_linelen >= 4 && strncmp(firstline, "HEAD", 4) == 0) ||
+		(first_linelen >= 4 && strncmp(firstline, "LOCK", 4) == 0) ||
+		(first_linelen >= 4 && strncmp(firstline, "MOVE", 4) == 0) ||
+		(first_linelen >= 4 && strncmp(firstline, "POLL", 4) == 0) ||
+		(first_linelen >= 4 && strncmp(firstline, "POST", 4) == 0) ||
+        (first_linelen >= 5 && strncmp(firstline, "BCOPY", 5) == 0) ||
+		(first_linelen >= 5 && strncmp(firstline, "BMOVE", 5) == 0) ||
+		(first_linelen >= 5 && strncmp(firstline, "MKCOL", 5) == 0) ||
+		(first_linelen >= 5 && strncmp(firstline, "TRACE", 5) == 0) ||
+		(first_linelen >= 5 && strncmp(firstline, "PATCH", 5) == 0) ||  
+		(first_linelen >= 5 && strncmp(firstline, "LABEL", 5) == 0) ||  
+		(first_linelen >= 5 && strncmp(firstline, "MERGE", 5) == 0) ||
+        (first_linelen >= 6 && strncmp(firstline, "DELETE", 6) == 0) ||
+		(first_linelen >= 6 && strncmp(firstline, "SEARCH", 6) == 0) ||
+		(first_linelen >= 6 && strncmp(firstline, "UNLOCK", 6) == 0) ||
+		(first_linelen >= 6 && strncmp(firstline, "REPORT", 6) == 0) || 
+		(first_linelen >= 6 && strncmp(firstline, "UPDATE", 6) == 0)) {
+            
+        return true;
     }
-    return tvb_captured_length(tvb);
+    else {
+        return false
+    }
 }
 
 void
 proto_register_ippusb(void)
 {
-    //static hf_register_info hf[] = {
-     // /* Generated from convert_proto_tree_add_text.pl */
-   // };
 
-   // static gint *ett[] = {
-   //     &ett_ippusb,
-   //     &ett_ippusb_as,
-   //     &ett_ippusb_attr,
-   //     &ett_ippusb_member
-   // };
+   static gint *ett[] = {
+        &ett_ippusb,
+        &ett_ippusb_as,
+        &ett_ippusb_attr,
+        &ett_ippusb_member
+    };
 
     proto_ippusb = proto_register_protocol("Internet Printing Protocol Over USB", "IPPUSB", "ippusb");
 
     ippusb_dissector_table = register_dissector_table("ippusb", "IPP Over USB", proto_ippusb, FT_UINT8, BASE_DEC);
 
-    //proto_register_field_array(proto_ippusb, hf, array_length(hf));
-    //proto_register_subtree_array(ett, array_length(ett));
+    proto_register_subtree_array(ett, array_length(ett));
 }
 
  void
@@ -93,8 +129,10 @@ proto_reg_handoff_ippusb(void)
     dissector_handle_t ippusb_handle;
 
     /*
-     * Register ourselves as running atop HTTP and using port 631.
+     * Register ourselves under usb bulk 
+     * IPP packets could come from a variety of usb class types
      */
     ippusb_handle = create_dissector_handle(dissect_ippusb, proto_ippusb);
-    dissector_add_uint("usb.bulk", IF_CLASS_PRINTER, ippusb_handle);
+    dissector_add_uint("usb.bulk", IF_CLASS_UNKNOWN, ippusb_handle);
+    dissector_add_uint("usb.bulk", IF_CLASS_VENDOR_SPECIFIC, ippusb_handle);
 }
